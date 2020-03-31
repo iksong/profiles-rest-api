439e6fd1bb070bae2de311ed72f33d313eaef9d6
IK Song <lcdf17@gmail.com>
2020-03-31 12:10:34 -0500

    added profiles viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,7 +31,7 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-class UserProfile (AbstractBaseUser, PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
